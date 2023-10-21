cf089bc012955cb09e584bb4d5c0316473849c05
muriloeduardo <muriloeduardoooooo@gmail.com>
2023-10-21 01:16:58 -0300

    Add messageSchema

--- models/Message.js
+++ models/Message.js
@@ -1,6 +1,28 @@
 const mongoose = require('mongoose');
 
-const messageSchema = new mongoose.Schema();
+const messageSchema = new mongoose.Schema({
+    headers: Object,
+    body: {
+        object: String,
+        entry: [
+            {
+                id: String,
+                changes: [
+                    {
+                        value: {
+                            messaging_product: String,
+                            metadata: {
+                                display_phone_number: String,
+                                phone_number_id: String,
+                            },
+                        },
+                        field: String,
+                    },
+                ],
+            },
+        ],
+    },
+});
 
 const MessageModel = mongoose.model('messages', messageSchema);
 
